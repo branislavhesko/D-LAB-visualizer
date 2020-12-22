8de915a5add58ccd6a4bc63ade1675a570e20d1a
Branislav Hesko <branislav.hesko@protonmail.ch>
2020-12-22 14:44:42 +0100

    update

--- synchronization_manager.py
+++ synchronization_manager.py
@@ -1,7 +1,30 @@
+import os
+
+import numpy as np
 import pandas as pd
 
 
 class SynchronizationManager:
 
     def __init__(self, path_to_synchronization_file):
-        pass
+        self._synchronization_time = None
+        self._load(path_to_synchronization_file)
+
+    @property
+    def synchronization_time(self):
+        return self._synchronization_time / 1000. if self._synchronization_time is not None else None
+
+    def _load(self, path):
+        if not os.path.exists(path):
+            return
+        data = pd.read_csv(path, delimiter="\t")
+        if "client_utc" in data.columns and "server_utc" in data.columns:
+            self._synchronization_time = np.median(
+                data.server_utc.astype("int64").values - data.client_utc.astype("int64").values)
+        else:
+            pass
+
+
+if __name__ == "__main__":
+    sync = SynchronizationManager("./data/synchronization.txt")
+    print(sync.synchronization_time)
